9998c0d7f17e624c6816487584540e25d63c4621
Ramphys Tirana Brito <ramphystiranabrito@gmail.com>
2024-09-27 22:45:07 -0400

    UpDates

--- home/public/Js/scrolls.js
+++ home/public/Js/scrolls.js
@@ -33,7 +33,56 @@ window.addEventListener('load', function() {
       origin:'left',
       distance: '150px',
       reset: true
-  });
+  });sr.reveal('.blog1', {
+    duration: 1200,
+    origin: 'bottom',
+    distance: '250px',
+    delay: 0 // Aparece inmediatamente
+});
+// Blog
+sr.reveal('.blog2', {
+    duration: 1250,
+    origin: 'right',
+    distance: '350px',
+    delay: 200 // Retraso de 200 ms después del primero
+});
+sr.reveal('.blog3', {
+    duration: 1250,
+    origin: 'left',
+    distance: '350px',
+    delay: 400 // Retraso de 400 ms después del primero
+});
+sr.reveal('.blog4', {
+    duration: 1250,
+    origin: 'left',
+    distance: '350px',
+    delay: 600 // Retraso de 600 ms
+});
+sr.reveal('.blog5', {
+    duration: 1250,
+    origin: 'left',
+    distance: '350px',
+    delay: 800 // Retraso de 800 ms
+});
+sr.reveal('.blog6', {
+    duration: 1250,
+    origin: 'left',
+    distance: '350px',
+    delay: 1000 // Retraso de 1000 ms
+});
+sr.reveal('.blog7', {
+    duration: 1250,
+    origin: 'top',
+    distance: '350px',
+    delay: 1200 // Retraso de 1200 ms
+});
+sr.reveal('.blog8', {
+    duration: 1250,
+    origin: 'top',
+    distance: '350px',
+    delay: 1400 // Retraso de 1400 ms
+});
+
 
   sr.reveal('.ContactData', {
       duration: 2200,
