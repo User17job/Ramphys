69f7180ecd56551b3770e07ec36d397227706f4d
User17job <User17job@gmail.com>
2024-11-04 02:06:53 -0400

    cambios en algunos links

--- home/public/Js/externalLinks.js
+++ home/public/Js/externalLinks.js
@@ -1,43 +1,40 @@
 function navigateTo(page) {
-    const urls = {
-        // 'home': 'https://ramphystb.netlify.app/',
-        'about': 'https://ramphystb-about.netlify.app/',
-        'portfolio': 'https://ramphystb-portafolio.netlify.app/',
-        'services': 'https://ramphystb-service.netlify.app/',
-        'contact': 'https://ramphystb-contact.netlify.app/',
-        'blog': 'https://user17job.systeme.io/unprogramayyo',
-        'hire': 'https://ramphystb-contact.netlify.app/',
+  const urls = {
+    // 'home': 'https://ramphystb.netlify.app/',
+    about: "https://ramphystb-about.netlify.app/",
+    portfolio: "https://ramphystb-portafolio.netlify.app/",
+    services: "https://ramphystb-service.netlify.app/",
+    contact: "https://ramphystb-contact.netlify.app/",
+    blog: "https://user17job.systeme.io/unprogramayyo",
+    hire: "https://ramphystb-contact.netlify.app/",
 
-        'download_cv': 'https://github.com/user17job/user17job',
-        'cv': 'https://github.com/user17job',
+    download_cv: "https://github.com/user17job/user17job",
+    cv: "https://github.com/user17job",
 
-        'linkedin': 'https://www.linkedin.com/in/user17job',
-        'github': 'https://github.com/user17job',
-        'instagram': 'https://www.instagram.com//ramphys_/',
-        'freecodecamp': 'https://www.freecodecamp.org/user17job',
-        'user17job':'https://user17job.github.io/User17job.inc./',
+    linkedin: "https://www.linkedin.com/in/user17job",
+    github: "https://github.com/user17job",
+    instagram: "https://www.instagram.com//ramphys_/",
+    freecodecamp: "https://www.freecodecamp.org/user17job",
+    user17job: "https://user17job.github.io/User17job.inc./",
 
-        'llamar':'tel:+18295237077',
-        'email':'mailto:ramphys.g.c.p@gmail.com',
-        'whatsapp':'https://wa.link/wss1yb',
+    llamar: "tel:+18295237077",
+    email: "mailto:ramphys.g.c.p@gmail.com",
+    whatsapp: "https://wa.link/wss1yb",
 
-        "project1":"http://127.0.0.1:5500/home/index.html",
-        "project2":"http://127.0.0.1:5500/project2/index.html",
-        "project3":"http://127.0.0.1:5500/project3/index.html",
-        "project4":"http://127.0.0.1:5500/project4/index.html",
-    };
+    project1: "http://127.0.0.1:5500/home/index.html",
+    project2: "http://127.0.0.1:5500/project2/index.html",
+    project3: "http://127.0.0.1:5500/project3/index.html",
+    project4: "http://127.0.0.1:5500/project4/index.html",
+  };
 
-    // window.location.href = urls[page], '_blank';
+  // window.location.href = urls[page], '_blank';
 
-    if (urls[page]) {
-        window.open(urls[page], '_blank');
-    } else {
-        console.error('URL not found for platform:', page);
-    }
+  if (urls[page]) {
+    window.open(urls[page], "_blank");
+  } else {
+    console.error("URL not found for platform:", page);
+  }
 }
-
-
-
 
 // if (urls[platform]) {
 //     window.open(urls[platform], '_blank');
